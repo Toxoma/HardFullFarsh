7eb9154101bebb9aa1bdf7fed3e5e04fad805809
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-05-28 11:28:23 +0300

    reduce()

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+"use strict";
+
 const num = 266219;
-const a = num.toString();
-let b = 1;
-for (let i = 0; i < a.length; i++) {
-   b *= a[i];
-}
-console.log(b);
-b = b ** 3;
-console.log(b.toString().substr(0, 2));
+// const a = num.toString();
+// let b = 1;
+let c = Array.from(num.toString(), Number);
+
+let result = c.reduce((sum, cur) => {
+   return sum * cur;
+});
+console.log(result);
+
+// for (let i = 0; i < a.length; i++) {
+//    b *= a[i];
+// }
+// console.log(b);
+
+result = result ** 3;
+console.log(result.toString().substr(0, 2));

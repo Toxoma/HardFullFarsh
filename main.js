535471dbf7d2e4a2b49375485f82a57640488adb
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-01 10:25:51 +0300

    lesson04

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 "use strict";
 
-// const arr = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
-// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-let lang = prompt("'ru' или 'en'", 'ru');
-
-// 1ый
-if (lang === "ru") {
-   console.log('"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"');
-} else {
-   console.log('"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"');
-}
-
-// 2ой
-switch (lang) {
-   case "ru":
-      console.log('"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"');
-      break;
-   case "en":
-      console.log('"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"');
-}
-
-// 3ий
-const mas = {
-   ru: '"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"',
-   en: '"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"'
+let str = ' 123456789012345678901234567890abcd';
+const first = function (a) {
+   if (typeof (a) !== 'string') {
+      return ('Это не строка!!!');
+   }
+   a = a.trim();
+   if (a.length > 30) {
+      return a.substring(0, 30) + '...';
+   } else {
+      return a;
+   }
 };
-console.log(mas[lang]);
-//_____________________________________________
-let namePerson = prompt('Введите имя!');
-console.log(namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент');
+console.log(first(str));

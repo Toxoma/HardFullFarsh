6af5b9a493c3d3d896776ffa78e04856d793c2a4
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-02 20:38:21 +0300

    lesson05

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 "use strict";
 
-let str = ' 123456789012345678901234567890abcd';
-const first = function (a) {
-   if (typeof (a) !== 'string') {
-      return ('Это не строка!!!');
+let arr = [
+   '122',
+   '223',
+   '224',
+   '125',
+   '426',
+   '127',
+   '428',
+];
+
+for (let i = 0; i < arr.length; i++) {
+   if (arr[i].substr(0, 1) === "2" || arr[i].substr(0, 1) === "4") {
+      console.log(arr[i]);
    }
-   a = a.trim();
-   if (a.length > 30) {
-      return a.substring(0, 30) + '...';
-   } else {
-      return a;
+}
+//_________________________
+function search(a) {
+   for (let j = a - 1; j > 1; j--) {
+      if (a % j === 0) {
+         return;
+      }
    }
-};
-console.log(first(str));
+   console.log(a + " : Делители этого числа: 1 и " + a);
+}
+
+for (let i = 1; i <= 100; i++) {
+   search(i);
+}
+

0d42031989374ef7b757f5e512848aa8be94b9bb
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-04 12:24:10 +0300

    lesson07

--- main.js
+++ main.js
@@ -1,62 +1,14 @@
-"use strict";
+let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
-let isNumber = function (n) {
-   const a = !isNaN(parseFloat(n)) && isFinite(n) && +n >= 1 && +n <= 100;
-   if (!a) {
-      alert('Введи число!');
+let today = new Intl.DateTimeFormat('ru-RU', { weekday: 'long' }).format(new Date());
+
+for (let item in week) {
+   let x = week[item];
+   if (week[item] === "суббота" || week[item] === "воскресенье") {
+      x = x.toUpperCase();
    }
-   return a;
-};
-
-function getRandomArbitrary(min = 1, max = 100) {
-   return Math.random() * (max - min) + min;
+   if (week[item] === today) {
+      x = x.bold();
+   }
+   console.log(x);
 }
-
-function start() {
-
-   function endProg(a) {
-      if (a === true) {
-         start();
-      }
-   }
-
-   let y = getRandomArbitrary();
-   y = Math.floor(y);
-   console.log(y);
-
-   let n = 3;
-
-   function chislo() {
-      let x;
-      let a = false;
-      do {
-         x = prompt('Угадай число от 1 до 100');
-         if (x === null) {
-            return alert('Игра окончена');
-         }
-         if (n === 1) {
-            a = confirm('Попытки закончились, хотите сыграть еще?')
-            endProg(a);
-            return;
-         }
-      } while (!isNumber(x));
-
-      if (+x > y) {
-         n--;
-         alert("Загаданное число меньше, осталось попыток " + n);
-         chislo();
-      }
-      if (+x < y) {
-         n--;
-         alert("Загаданное число больше, осталось попыток " + n);
-         chislo();
-      }
-      if (+x === y) {
-         a = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-         endProg(a);
-      }
-   }
-   chislo();
-}
-
-start();

fcfa4ae63ac1c381fc8ac10268b481a5eb4fe7ea
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-07 15:07:47 +0300

    lesson09

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
-let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+"use strict";
 
-let today = new Intl.DateTimeFormat('ru-RU', { weekday: 'long' }).format(new Date());
+const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
-for (let item in week) {
-   let x = week[item];
-   if (week[item] === "суббота" || week[item] === "воскресенье") {
-      x = x.toUpperCase();
+setInterval(() => {
+
+   let day = new Intl.DateTimeFormat('ru-RU', { weekday: 'long' }).format(new Date());
+
+   day = day[0].toUpperCase() + day.substr(1);
+
+   let nowTime = new Date();
+
+   let first = document.querySelector('#first');
+
+   function changer(hour) {
+      switch (hour) {
+         case 1, 21:
+            return 'час';
+
+         case 2, 3, 4, 22, 23, 24:
+            return 'часа';
+
+         default:
+            return 'часов';
+      }
    }
-   if (week[item] === today) {
-      x = x.bold();
+
+   let text = `a) 'Сегодня ${day}, ${nowTime.getDay()} ${months[nowTime.getMonth()]} ${nowTime.getFullYear()} года, ${nowTime.getHours()} ${changer(nowTime.getHours())} ${nowTime.getMinutes()} минут ${nowTime.getSeconds()} секунды'`;
+
+   first.innerHTML = text;
+
+
+   let second = document.querySelector('#second');
+
+   function addNull(item) {
+      item = item.split('.');
+
+      for (let i in item) {
+         if (item[i].length < 2) {
+            item[i] = '0' + item[i];
+         }
+      }
+      item = item.join(':');
+
+      return item.replace(':', '.').replace(':', '.').replace(':', ' - ');
    }
-   console.log(x);
-}
+
+   text = `б) '${addNull(nowTime.toLocaleString().replace(', ', '.').replaceAll(':', '.'))}'`;
+
+   second.innerHTML = text;
+}, 1000);
